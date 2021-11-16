8f3ae91461071f19dcb5507e397b61263f169033
Jay <nhanlnt@hotmail.com>
2021-11-16 11:32:41 +0700

    [University]: Complete create university route

--- server/src/routes/Education.ts
+++ server/src/routes/Education.ts
@@ -1,80 +1,39 @@
-import express, { Request, Response } from 'express';
-import { db } from '../models';
+import {
+  createMultiEducationData,
+  createNewEducationData,
+  deleteEducationData,
+  getAllEducationData,
+  patchEducationData,
+} from './../controllers/Education';
+import express from 'express';
+import { body, check } from 'express-validator';
 
 const educationRouter = express.Router();
-const { UniversityData } = db;
-
-interface IEducationData {
-  name: string;
-  code: string;
-  location: string;
-}
 
 // Get university or college
-educationRouter.get('/', async (req: Request, res: Response) => {
-  try {
-    const educationData = await UniversityData.findAll();
-    res.status(200).json({
-      success: true,
-      data: educationData,
-    });
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+educationRouter.get('/', getAllEducationData);
 
-// Add a new university or college
-educationRouter.post('/', async (req: Request, res: Response) => {
-  const educationInput: IEducationData = req.body;
-  const name = educationInput.name.trim();
-  const code = educationInput.code.trim();
-  const location = educationInput.location.trim();
-  const recordCheck = await UniversityData.findOne({ where: { name: name } });
-  if (!name) {
-    res.status(400).json({ error: 'University/College name cannot be empty.' });
-  }
-  if (!code) {
-    res.status(400).json({ error: 'University/College code cannot be empty.' });
-  }
-  if (!location) {
-    res.status(400).json({
-      error: 'University/College location cannot be empty.',
-    });
-  }
-  if (name && code && location) {
-    if (!recordCheck) {
-      try {
-        const response = await UniversityData.create({ name, location, code });
-        res.status(201).json({
-          success: true,
-          message: `${response.name} is created.`,
-          data: response,
-        });
-      } catch (error) {
-        res.status(400).json(error);
-      }
-    } else {
-      res.status(400).json({ error: `${name} already exist.` });
-    }
-  }
-});
+// Create a new university or college
+educationRouter.post(
+  '/',
+  body('name').notEmpty().trim(),
+  body('code').notEmpty().trim(),
+  body('location').notEmpty().trim(),
+  createNewEducationData,
+);
 
-// Add multi new university or college
-educationRouter.post('/create-multi', async (req: Request, res: Response) => {
-  const educationInput: IEducationData[] = req.body;
-  try {
-    educationInput.map(async (val) => {
-      const name = val.name.trim();
-      const code = val.code.trim();
-      const location = val.location.trim();
-      await UniversityData.create({ name, code, location });
-    });
-    res.status(201).json({
-      success: true,
-    });
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+// Create multi new university or college using json
+educationRouter.post('/create-multi', createMultiEducationData);
+
+// Put university or college
+educationRouter.patch(
+  '/update/code=:code',
+  check('name').optional().if(body('name').exists()).notEmpty().trim(),
+  check('location').optional().if(body('location').exists()).notEmpty().trim(),
+  patchEducationData,
+);
+
+// Put university or college
+educationRouter.delete('/delete/code=:code', deleteEducationData);
 
 export { educationRouter };
